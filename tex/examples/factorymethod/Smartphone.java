b54b8738966da5fa3aed11057a0b0b171fd04c51
adrianlehmann <info@adrianlehmann.net>
2017-07-09 19:08:31 +0200

    Added factory method notes

---
+++ tex/examples/factorymethod/Smartphone.java
@@ -0,0 +1,8 @@
+package net.adrianlehmann.swt_revision.patterns.variation_patterns.factory_method;
+
+/**
+ * Created by adrianlehmann on 09.07.17.
+ */
+public interface Smartphone {
+    //Do things
+}
