db664139ea77dea201088d741a21cca70cf95504
adrianlehmann <info@adrianlehmann.net>
2017-07-09 21:24:00 +0200

    Added abstract factory notes

---
+++ tex/examples/abstractfactory/NavBar.java
@@ -0,0 +1,8 @@
+package net.adrianlehmann.swt_revision.patterns.variation_patterns.abstractfactory;
+
+/**
+ * Created by adrianlehmann on 09.07.17.
+ */
+public interface NavBar {
+    void paint();
+}
